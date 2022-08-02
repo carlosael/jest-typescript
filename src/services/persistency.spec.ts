325035c1c7dcd1f21c554f1b33f9b7e74d305d98
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-08-02 18:25:21 -0300

    feat: persistency.spec.ts added

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,24 @@
+import { Persistency } from './persistency';
+
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Order saved"', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Order saved');
+  });
+});
