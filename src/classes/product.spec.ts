6a24a4395a2a9baec25c89b4628463b61062d329
Carlos Alberto <86244416+carlosael@users.noreply.github.com>
2022-08-02 18:44:36 -0300

    feat: product.spec.ts added

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,15 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number) => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    const sut = createSut('Shirt', 10);
+    expect(sut).toHaveProperty('name', 'Shirt');
+    expect(sut).toHaveProperty('price', 10);
+  });
+});
